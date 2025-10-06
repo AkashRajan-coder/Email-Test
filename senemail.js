5ac65da833e250642429aaa8a1028900b399de0f
Akash <rajanakash331@gmail.com>
2025-10-06 07:33:16 +0530

    Create senemail.js

---
+++ senemail.js
@@ -0,0 +1,17 @@
+import transporter from "../server/controller/transporter.js";
+
+export const sendTestEmail = async () => {
+  try {
+    await transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to: "rajanakash331@gmail.com",
+      subject: "Test Email from Render",
+      text: "Hello! This is a test email.",
+    });
+    console.log("Email sent successfully!");
+  } catch (err) {
+    console.error("Email send failed:", err);
+  }
+};
+
+sendTestEmail();
